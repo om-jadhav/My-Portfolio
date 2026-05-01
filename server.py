c6438e672c2c411bda14d7b3940df18c70c3cd23
om-jadhav <om.jadhav2091@gmail.com>
2026-05-01 14:53:54 +0530

    updated Server.py

--- server.py
+++ server.py
@@ -75,4 +75,4 @@ def submit_form():
         return "Something went wrong while saving the message."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
